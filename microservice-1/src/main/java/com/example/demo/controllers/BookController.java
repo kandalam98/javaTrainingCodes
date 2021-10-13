3542bdaebe0eb829247324a0877ec5d5c46e2f1b
srikar kandalam <kvps148@gmail.com>
2021-10-13 16:11:46 +0530

    ms1

---
+++ microservice-1/src/main/java/com/example/demo/controllers/BookController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookController {
+	@Value("${server.port}")
+	private String port;
+	
+	@GetMapping(path="/books")
+	public String getBooks() {
+		return "Head first Java,Effective java" + this.port;
+	}
+
+}
