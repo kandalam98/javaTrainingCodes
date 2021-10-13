d00e1d985c278ddf10900166ee38fb7078963b90
srikar kandalam <kvps148@gmail.com>
2021-10-13 15:48:37 +0530

    threads

---
+++ threads_example/src/PrintStrings.java
@@ -0,0 +1,13 @@
+
+
+public class PrintStrings {
+	public synchronized static void print(String str1 , String str2) {
+		System.out.println(str1);
+		try {
+			Thread.sleep(500);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(str2);
+	}
+}
