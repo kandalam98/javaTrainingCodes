fa833e00335d4e2634e0627922ef8819238e8e0a
srikar kandalam <kvps148@gmail.com>
2021-10-13 16:12:43 +0530

    mc reactive client

---
+++ microservices-reactive-client/src/main/java/com/example/demo/MicroservicesReactiveClientApplication.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@SpringBootApplication
+public class MicroservicesReactiveClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesReactiveClientApplication.class, args);
+	}
+	
+	@Bean
+	@LoadBalanced
+	public WebClient.Builder builder(){
+		return WebClient.builder();
+	}
+	
+	@Bean
+	
+	public WebClient client(WebClient.Builder builderRef) {
+		return builderRef.build();
+	}
+
+}
